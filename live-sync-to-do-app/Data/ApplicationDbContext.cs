b4eb3eac9058d9571293297538934f06179b2020
F-Kowal <fkowal.krk@gmail.com>
2026-01-04 15:05:42 +0100

    initial commit

---
+++ live-sync-to-do-app/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace live_sync_to_do_app.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
---
+++ live-sync-to-do-app/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace live_sync_to_do_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
